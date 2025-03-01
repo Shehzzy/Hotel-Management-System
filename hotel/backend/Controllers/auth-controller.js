59153e261746c64e78a1a311ee6eb3cc39d766a5
Shehzzy <aptechsheh@gmail.com>
2025-03-01 11:07:04 +0500

    Auth controller APIS done.

--- hotel/backend/Controllers/auth-controller.js
+++ hotel/backend/Controllers/auth-controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../Models/UserModel");
-// USER REGISTRATION API
+const jwt = require("jsonwebtoken");
 
+// USER REGISTRATION API
 const register = async (req, res) => {
   try {
     const { fullname, username, phone, email, password, role } = req.body;
@@ -19,8 +20,18 @@ const register = async (req, res) => {
       role: role,
     });
 
+    const token = jwt.sign(
+      {
+        userId: newUser._id,
+        email: newUser.email,
+        role: newUser.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
     if (newUser) {
-      return res.json({ message: "User has been created successfully" });
+      return res.json({ message: "User has been created successfully", token });
     }
   } catch (error) {
     console.log("An error occurred", error.message);
@@ -28,5 +39,81 @@ const register = async (req, res) => {
   }
 };
 
+// LOGIN API
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
-module.exports = {register};
+    if (!user) {
+      return res.json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return res.json({ message: "User logged in successfully", token });
+  } catch (error) {
+    console.log("An error occurred in login API", error.message);
+    return res.json({ message: "An error occurred", error });
+  }
+};
+
+// PROFILE API
+
+const profileData = async (req, res) => {
+  try {
+    const findUser = await userModel.findOne({ _id: req.user.userId });
+
+    if (!findUser) {
+      return res.json({ message: "Unable to find User" });
+    }
+
+    var userData = {
+      fullname: findUser.fullName,
+      email: findUser.email,
+      username: findUser.username,
+      phone: findUser.phone,
+      role: findUser.role,
+      password: findUser.password,
+    };
+
+    return res.json({ message: "Here is your user data", userData });
+  } catch (error) {
+    console.log("An error occurred in profile API", error.message);
+    return res.json({ message: "An error occurred", error });
+  }
+};
+
+// Update API
+const update = async (req, res) => {
+  try {
+    const updateUser = await userModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updateUser) {
+      return res.json({ message: "An error occurred while updating data." });
+    }
+
+    return res.json({ message: "Records updated successfully", updateUser });
+  } catch (error) {
+    console.log("An error occurred in update API", error.message);
+    return res.json({ message: "An error occurred", error });
+  }
+};
+module.exports = { register, login, profileData, update };
--- hotel/backend/Routes/auth-router.js
+++ hotel/backend/Routes/auth-router.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const authController = require("../Controllers/auth-controller");
 
 router.post("/register", authController.register);
+router.get("/profile", authController.profileData);
+router.post("/login", authController.login);
+router.put("/update/:id", authController.update);
 
 module.exports = router;
